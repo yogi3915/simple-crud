d4e202b850e5d01b44d3a46d91eef876f46601cc
yogi3915 <yogi.ferdian3915@gmail.com>
2022-01-24 06:11:15 +0700

    pindah file

--- application/src/App.js
+++ application/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>create by: Yogi</p>
+        <p>create by: Yogi.....</p>
         <a
           className="App-link"
           href="https://reactjs.org"
